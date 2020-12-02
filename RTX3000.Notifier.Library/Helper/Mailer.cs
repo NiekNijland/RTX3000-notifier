64321b075a874b89a702ff28a99a323c74d7562b
Mark Heinis <heinis.mark@gmail.com>
2020-12-02 10:00:18 +0100

    ...

--- RTX3000.Notifier.Library/Helper/Mailer.cs
+++ RTX3000.Notifier.Library/Helper/Mailer.cs
@@ -3,7 +3,6 @@ using System;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading;
-using System.Xml;
 using Windows.System;
 using Windows.UI.Notifications;
 
@@ -14,12 +13,16 @@ namespace RTX3000.Notifier.Library.Helper
     /// </summary>
     public static class Mailer
     {
-        #region Public
+        #region Variables
 
         /// <summary>
-        /// 
+        /// Defines the ProductUrl.
         /// </summary>
         public static String ProductUrl = "";
+
+        #endregion
+
+        #region Public
 
         /// <summary>
         /// Send notifications threaded.
@@ -99,6 +102,45 @@ namespace RTX3000.Notifier.Library.Helper
             SendMail(Constants.GetErrorLogAddress(), subject, body);
         }
 
+        /// <summary>
+        /// Send the actual email to the smtp server.
+        /// </summary>
+        /// <param name="subject">The subject<see cref="string"/>.</param>
+        /// <param name="body">The body<see cref="string"/>.</param>
+        public static void SendToast(string subject, string body = null)
+        {
+            if (!Constants.GetUseToasts())
+                return;
+
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var textNodes = template.GetElementsByTagName("text").ToList();
+            foreach (var textNode in textNodes)
+            {
+                textNode.AppendChild(template.CreateTextNode(body));
+            }
+
+            var toast = new ToastNotification(template)
+            {
+                Tag = "RTX 30XX Notifier",
+                Group = "RTX30XX",
+                ExpirationTime = DateTimeOffset.Now.AddMinutes(5),
+            };
+
+            toast.Activated += toast_Activated;
+            var notifier = ToastNotificationManager.CreateToastNotifier(subject);
+            notifier.Show(toast);
+        }
+
+        /// <summary>
+        /// The toast_Activated.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="ToastNotification"/>.</param>
+        /// <param name="args">The args<see cref="object"/>.</param>
+        public async static void toast_Activated(ToastNotification sender, object args)
+        {
+            await Launcher.LaunchUriAsync(new Uri(!string.IsNullOrEmpty(ProductUrl) ? ProductUrl : "https://geforce.nieknijland.com/"));
+        }
+
         #endregion
 
         #region Private
@@ -136,40 +178,6 @@ namespace RTX3000.Notifier.Library.Helper
             {
                 Logger.EmailError(email);
             }
-        }
-
-        /// <summary>
-        /// Send the actual email to the smtp server.
-        /// </summary>
-        /// <param name="subject">The subject<see cref="string"/>.</param>
-        /// <param name="body">The body<see cref="string"/>.</param>
-        public static void SendToast(string subject, string body = null)
-        {
-            if (!Constants.GetUseToasts())
-                return;
-
-            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
-            var textNodes = template.GetElementsByTagName("text").ToList();
-            foreach (var textNode in textNodes)
-            {
-                textNode.AppendChild(template.CreateTextNode(body));
-            }
-
-            var toast = new ToastNotification(template)
-            {
-                Tag = "RTX 30XX Notifier",
-                Group = "RTX30XX",
-                ExpirationTime = DateTimeOffset.Now.AddMinutes(5),
-            };
-
-            toast.Activated += toast_Activated;
-            var notifier = ToastNotificationManager.CreateToastNotifier(subject);
-            notifier.Show(toast);
-        }
-
-        public async static void toast_Activated(ToastNotification sender, object args)
-        {
-            await Launcher.LaunchUriAsync(new Uri(!string.IsNullOrEmpty(ProductUrl) ? ProductUrl : "https://geforce.nieknijland.com/"));
         }
 
         #endregion
